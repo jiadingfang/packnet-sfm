1946ad91ff3a6252417a3d49f4e005e2234fe5c5
kudo1026 <gfjd003@gmail.com>
2021-08-15 06:31:39 +0000

    add eucm model

---
+++ packnet_sfm/networks/layers/resnet/eucm_decoder.py
@@ -0,0 +1,92 @@
+# Copyright 2020 Toyota Research Institute.  All rights reserved.
+
+# Adapted from monodepth2
+# https://github.com/nianticlabs/monodepth2/blob/master/networks/depth_decoder.py
+
+from __future__ import absolute_import, division, print_function
+
+import numpy as np
+import torch
+import torch.nn as nn
+
+from collections import OrderedDict
+from .layers import ConvBlock, Conv3x3, upsample
+
+
+class EUCMDecoder(nn.Module):
+    def __init__(self, num_ch_enc, scales=[0], num_output_channels=3, use_skips=True):
+        super(EUCMDecoder, self).__init__()
+
+        self.num_output_channels = num_output_channels
+        self.use_skips = use_skips
+        self.upsample_mode = 'nearest'
+        self.scales = scales
+
+        self.num_ch_enc = num_ch_enc
+        self.num_ch_dec = np.array([16, 32, 64, 128, 256])
+
+        # camera intrinsic parameter as a vector
+        # self.intrinsic_vector = nn.Parameter(torch.zeros(6))
+        self.intrinsic_vector = nn.Parameter(-torch.ones(6))
+
+        # decoder
+        self.convs = OrderedDict()
+        for i in range(4, -1, -1):
+            # upconv_0
+            num_ch_in = self.num_ch_enc[-1] if i == 4 else self.num_ch_dec[i + 1]
+            num_ch_out = self.num_ch_dec[i]
+            self.convs[("upconv", i, 0)] = ConvBlock(num_ch_in, num_ch_out)
+
+            # upconv_1
+            num_ch_in = self.num_ch_dec[i]
+            if self.use_skips and i > 0:
+                num_ch_in += self.num_ch_enc[i - 1]
+            num_ch_out = self.num_ch_dec[i]
+            self.convs[("upconv", i, 1)] = ConvBlock(num_ch_in, num_ch_out)
+
+        for s in self.scales:
+            self.convs[("dispconv", s)] = Conv3x3(self.num_ch_dec[s], self.num_output_channels)
+
+        # self.convs['conv1'] = ConvBlock(512, 1)
+        # # self.convs['linear_focal'] = nn.Linear(int(128 * 128 / 1024), 2)
+        # # self.convs['linear_offset'] = nn.Linear(int(128 * 128 / 1024), 2)
+        # # self.convs['linear_alpha'] = nn.Linear(int(128 * 128 / 1024), 1)
+        # self.convs['linear_focal'] = nn.Linear(int(192 * 640 / 1024), 2)
+        # self.convs['linear_offset'] = nn.Linear(int(192 * 640 / 1024), 2)
+        # self.convs['linear_alpha'] = nn.Linear(int(192 * 640 / 1024), 1)
+        # # self.convs['linear_focal'] = nn.Linear(int(384 * 384 / 1024), 2)
+        # # self.convs['linear_offset'] = nn.Linear(int(384 * 384 / 1024), 2)
+        # # self.convs['linear_alpha'] = nn.Linear(int(384 * 384 / 1024), 1)
+        # self.convs['activation_focal'] = nn.Tanh()
+        # self.convs['activation_offset'] = nn.Tanh()
+        # self.convs['activation_alpha'] = nn.Tanh()
+        # # self.convs['activation_alpha'] = nn.Sigmoid()
+
+        # self.decoder = nn.ModuleList(list(self.convs.values()))
+        self.tanh = nn.Tanh()
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, input_features):
+        self.output = {}
+
+        # get forcal length and offsets
+        x = input_features[-1]
+        B = x.shape[0]
+        
+        fx, fy, cx, cy = self.sigmoid(self.intrinsic_vector[0:4]) * 1000
+        alpha = self.sigmoid(self.intrinsic_vector[4]) * 1 / 2
+        # alpha = 0
+        beta = self.sigmoid(self.intrinsic_vector[5]) * 2
+        # beta = 1
+
+        I = torch.zeros(6)
+        I[0] = fx
+        I[1] = fy
+        I[2] = cx
+        I[3] = cy
+        I[4] = alpha
+        I[5] = beta
+
+        self.output = I.unsqueeze(0).repeat(B,1)
+
+        return self.output
--- scripts/camera_test.py
+++ scripts/camera_test.py
@@ -1,6 +1,7 @@
 import torch
 from packnet_sfm.geometry.camera import Camera
 from packnet_sfm.geometry.camera_ucm import UCMCamera
+from packnet_sfm.geometry.camera_eucm import EUCMCamera
 
 K = torch.eye(3)
 K[0,0] = 5
@@ -9,17 +10,27 @@ K[0,2] = 96
 K[1,2] = 320
 K = K.unsqueeze(0)
 
-# I = torch.tensor([1,1,0,0,0])
-I = torch.tensor([5,8,96,320,0])
-I = I.unsqueeze(0)
+# I_ucm = torch.tensor([1,1,0,0,0])
+I_ucm = torch.tensor([5,8,96,320,0])
+I_ucm = I_ucm.unsqueeze(0)
+
+# I_eucm = torch.tensor([1,1,0,0,0,1])
+I_eucm = torch.tensor([5,8,96,320,0,1])
+I_eucm = I_eucm.unsqueeze(0)
 
 camera = Camera(K=K)
-ucm_camera = UCMCamera(I=I)
+ucm_camera = UCMCamera(I=I_ucm)
+eucm_camera = EUCMCamera(I=I_eucm)
 
 x = torch.rand(1,3,1,1)
 d = torch.rand(1,1,1,1)
 
+print('projection')
 print(camera.project(x))
 print(ucm_camera.project(x))
+print(eucm_camera.project(x))
+print()
+print('unprojection')
 print(camera.reconstruct(d))
 print(ucm_camera.reconstruct(d))
+print(eucm_camera.reconstruct(d))
